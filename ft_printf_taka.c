616fd554a7c7b1f328341a971a41f07e5f8e5fc8
taka2162 <takat.takat@outlook.com>
2024-05-06 13:30:13 +0900

    ft_put系関数が使えなかったのでコメントで記述 24/5/6

--- ft_printf_taka.c
+++ ft_printf_taka.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
-#include <io.h>
-#include <stdlib.h>
 
-int	ft_printf(char const *str, ...)
+int	ft_printf(const char *format, ...)
 {
-	printf("Hello world\n");
-	printf("branch???\n");
-	return (0);
+	va_list	args;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				//ft_putchar_fdで後ろの変数を標準出力に書く va_arg(args, int);
+			}
+			else if (*format == 'd')
+			{
+				//ft_putnbr_fdで後ろの変数を標準出力に書く va_arg(args, int);
+			}
+			else if (*format == 's')
+			{
+				//ft_putst5r_fdで後ろの変数を標準出力に書く va_arg(args, *char)
+			}
+			//ほかのフォーマットの処理も書く
+			else
+			{
+				//フォーマット指定子がわからない場合はエラーを返す（エラーメッセージを書くのかは不明）
+			}
+		}
+		else
+		{
+			//ft_putchar_fdでformatを書く
+		}
+	}
+	va_end(args); // すべての処理が終わったので引数のリストを閉じる
+	return (0)
 }
---
+++ libftprintf_taka.h
@@ -0,0 +1,9 @@
+#ifndef LIBFTPRINTF_H
+#define LIBFTPRINTF_H
+
+# include <stdlib.h>
+# include <stdrag.h>
+
+int	ft_printf(const char *fomat, ...);
+
+#endif
